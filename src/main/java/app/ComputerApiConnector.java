355b3df531a0e9cbfaad2ab34eef261b6df0a2cc
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-12-04 11:48:42 +0100

    Method deleteDataFromComputerApi() added in ComputerApiConnector Class

--- src/main/java/app/ComputerApiConnector.java
+++ src/main/java/app/ComputerApiConnector.java
@@ -3,6 +3,7 @@ package app;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,6 +24,19 @@ public class ComputerApiConnector {
                 Computer[].class);
 
         Stream.of(responseEntity.getBody()).forEach(System.out::println);
+    }
+
+    @GetMapping("/deleteDataFromComputerApi")
+    public void deleteDataFromComputerApi(@RequestParam long id){
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Boolean> responseEntity = restTemplate.exchange(
+                "http://localhost:8081/Gradle___pl_bartek_ficek___electronic_apps_1_0_SNAPSHOT_war__exploded_/api/computers"+"?id="+id,
+                HttpMethod.DELETE,
+                null,
+                Boolean.class);
+        System.out.println(responseEntity.getBody());
     }
 }
 
